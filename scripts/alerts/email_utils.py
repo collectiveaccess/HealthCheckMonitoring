d516ae7667e5d29cf049d1b1aa921dedfd6eaa24
CollectiveAccess <seth@collectiveaccess.org>
2024-07-18 15:25:13 -0400

    Replace email send function

--- scripts/alerts/email_utils.py
+++ scripts/alerts/email_utils.py
@@ -13,27 +13,45 @@ def send_email(subject, body, sender_email, receiver_email):
     msg["From"] = sender_email
     msg["To"] = receiver_email
     msg.set_content(body)
-
+    
     context = ssl.create_default_context()
+    server = smtplib.SMTP(os.environ.get("SMTP_SERVER"), os.environ.get("SMTP_PORT"))
+    server.ehlo()
+    server.starttls(context=context)
+    server.ehlo()
+    server.login( os.environ.get("SMTP_USERNAME"),  os.environ.get("SMTP_PASSWORD"))
 
-    with smtplib.SMTP_SSL(
-        os.environ.get("SMTP_SERVER"), os.environ.get("SMTP_PORT"), context=context
-    ) as server:
-        try:
-            server.login(
-                os.environ.get("SMTP_USERNAME"), os.environ.get("SMTP_PASSWORD")
-            )
-        except Exception as error:
-            print("could not login to email server.", error)
-            return
+    #for e in config['email_to']:
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+    server.quit()
 
-        try:
-            server.sendmail(sender_email, receiver_email, msg.as_string())
-        except Exception as error:
-            print(
-                f"could not send email. sender: {sender_email}, receiver: {receiver_email}.",
-                error,
-            )
+# def send_email(subject, body, sender_email, receiver_email):
+#     msg = EmailMessage()
+#     msg["Subject"] = subject
+#     msg["From"] = sender_email
+#     msg["To"] = receiver_email
+#     msg.set_content(body)
+# 
+#     context = ssl.create_default_context()
+# 
+#     with smtplib.SMTP_SSL(
+#         os.environ.get("SMTP_SERVER"), os.environ.get("SMTP_PORT"), context=context
+#     ) as server:
+#         try:
+#             server.login(
+#                 os.environ.get("SMTP_USERNAME"), os.environ.get("SMTP_PASSWORD")
+#             )
+#         except Exception as error:
+#             print("could not login to email server.", error)
+#             return
+# 
+#         try:
+#             server.sendmail(sender_email, receiver_email, msg.as_string())
+#         except Exception as error:
+#             print(
+#                 f"could not send email. sender: {sender_email}, receiver: {receiver_email}.",
+#                 error,
+#             )
 
 
 def send_status_email(project, status, reciever_email):
