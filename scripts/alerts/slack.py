74f57a49d8b5345423299f5275a4d6cf88863303
CollectiveAccess <seth@collectiveaccess.org>
2024-07-19 08:04:31 -0400

    Don't barf when Slack URL is not set

--- scripts/alerts/slack.py
+++ scripts/alerts/slack.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 
 def post_status_message(project, status, webhook_url):
+    if webhook_url is None: 
+        return
     if status == 0:
         data = {"text": f'{project["name"]} failed health check. {project["url"]}'}
     else:
